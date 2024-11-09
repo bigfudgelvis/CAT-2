617215b5fddef64914d8850f29142647b9fea1ad
bigfudgelvis <Elvismutai291@gmail.com>
2024-11-09 12:49:02 +0300

    Add files via upload

---
+++ CAT2 quiz1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+struct employee {
+    char name[25];
+    int id;
+    char department[20];
+    float salary;
+    char email[50];
+};
+
+int main() {
+    
+    struct employee emp;
+
+   
+    strcpy(emp.name, "John Doe");
+    emp.id = 12345;
+    strcpy(emp.department, "Human Resources");
+    emp.salary = 55000.50;
+    strcpy(emp.email, "john.doe@company.com");
+
+    
+    printf("Employee Details:\n");
+    printf("Name: %s\n", emp.name);
+    printf("ID: %d\n", emp.id);
+    printf("Department: %s\n", emp.department);
+    printf("Salary: %.2f\n", emp.salary);
+    printf("Email: %s\n", emp.email);
+
+    return 0;
+}
+/*expected output 
+Employee Details:
+Name: John Doe
+ID: 12345
+Department: Human Resources
+Salary: 55000.50
+Email: john.doe@company.com */
